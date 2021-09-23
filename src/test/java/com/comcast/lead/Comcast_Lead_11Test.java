67e47bc59618977cd8385a0804c20a1e0a953f6a
JunaidTy <91010549+JunaidTy@users.noreply.github.com>
2021-09-23 19:45:35 +0530

    Update Comcast_Lead_11Test.java

--- src/test/java/com/comcast/lead/Comcast_Lead_11Test.java
+++ src/test/java/com/comcast/lead/Comcast_Lead_11Test.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 public class Comcast_Lead_11Test {
 	@Test(groups = "SmokeSuite")
 	public void lead1() {
-		System.out.println("Lead test Case 1 executed");
+		System.out.println("Lead test Case smoke 1 executed");
 		
 	}
 	@Test(groups = "SmokeSuite")
 	public void lead2() {
-		System.out.println("Lead test Case 2 executed");
+		System.out.println("Lead test Case smoke 2 executed");
 		
 	}
 	@Test
